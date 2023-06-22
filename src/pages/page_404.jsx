bc277469dad4c58a0f3a6686115ad17ab9a919cc
Sergey Krivtsov <flashgangsta@gmail.com>
2023-06-22 02:15:24 +0200

    Some components root tag updates

--- src/pages/page_404.jsx
+++ src/pages/page_404.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Page404 = () => {
     return (
-        <div>
+        <>
             <h1>404</h1>
             <p>The requested URL<code>%PAGE_URL%</code> was not found</p>
-        </div>
+        </>
     );
 };
 
