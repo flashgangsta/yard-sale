14c16fb3cb18273a6aa1d45c128ff40276653de6
Sergey Krivtsov <flashgangsta@gmail.com>
2023-06-29 20:17:58 +0200

    Fixed translations loading (multiple rerender full app)

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,78 @@
 import "./styles/app.css";
 import {useEffect, useState} from "react";
+import {BrowserRouter} from "react-router-dom";
 import Api from "./api/Api";
+import Preloader from "./ui/Preloader";
+import Header from "./page_sections/header";
+import Router from "./Router";
+import Footer from "./page_sections/footer";
+import {LngContext} from "./context";
 
 function App() {
-    const [translates, setTranslates] = useState();
-    async function fetchTranslates() {
-        const json = await Api.get(Api.URL_LOCALIZATION + "en");
-        setTranslates(json);
-        return json;
+    const languages = {
+        en: "English",
+        ru: "Русский",
+        uk: "Українська",
+        sr: "Crnogorski"
+    };
+
+    const initialLng = localStorage.getItem("lang") || getSystemLng() || "en";
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [translations, setTranslations] = useState(null);
+    const [selectedLng, setSelectedLng] = useState(initialLng);
+
+
+    function getSystemLng() {
+        const navigatorLng = navigator.language.substring(0, 2).toLowerCase();
+        if(!languages.hasOwnProperty(navigatorLng)) {
+            return "en";
+        }
+        return navigatorLng;
     }
 
+    async function fetchTranslates(lng) {
+        const json = await Api.get(Api.URL_LOCALIZATION + lng);
+        setTranslations(json);
+        setSelectedLng(lng);
+        if(isLoading) {
+            setIsLoading(false);
+        }
+    }
+
+    function onLanguageChanged(lng) {
+        console.log("onLanguageChanged to " + lng);
+        fetchTranslates(lng);
+    }
+
+
     useEffect(() => {
-        fetchTranslates();
+        console.log("useEffect");
+        fetchTranslates(initialLng);
+        // eslint-disable-next-line
     }, []);
 
-    console.log("Hi!")
+    console.log("App");
 
     return (
-        <>Соси</>
+        <>
+            {isLoading
+                ? <Preloader/>
+                : <LngContext.Provider value={{
+                    selectedLng,
+                    languages,
+                    translations,
+                    onLanguageChanged
+                }}>
+                    <BrowserRouter>
+                        <Header/>
+                        <main>
+                            <Router/>
+                        </main>
+                        <Footer/>
+                    </BrowserRouter>
+                </LngContext.Provider>
+            }</>
     );
 }
 
