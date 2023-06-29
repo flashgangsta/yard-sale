6e6497d0d62466912cccedbd07f54e199655686a
Sergey Krivtsov <flashgangsta@gmail.com>
2023-06-29 06:28:48 +0200

    Added cors to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const cors = require("cors");
 let currentLocale;
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 
 app.get("/api/localization", (req, res) => {
     res.json({
@@ -18,6 +22,6 @@ app.get("/api/products", (req, res) => {
     })
 });
 
-app.listen(5000, () => {
-    console.log(`Server running http://localhost:${5000}`);
+app.listen(port, () => {
+    console.log(`Server running http://localhost:${port}`);
 })
